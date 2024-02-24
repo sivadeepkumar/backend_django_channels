ff97dcd6a9999349f8d6a967213ad1cf3ff25402
sivadeepkumar <sivadeepkumar3@gmail.com>
2024-02-25 00:32:29 +0530

    add admin.py file changes and created superuser

--- accounts/models.py
+++ accounts/models.py
@@ -12,7 +12,7 @@ class UserManager(BaseUserManager):
         return user 
     
 
-    def create_super(self,email,password = None ,**extra_fields):
+    def create_superuser(self,email,password = None ,**extra_fields):
         extra_fields.setdefault("is_staff",True)
         extra_fields.setdefault("is_superuser",True)
         return self.create_user(email,password,**extra_fields)
